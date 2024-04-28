084e1a5104c3a46091cccaa2e3bfec99cb077f4e
Lzsaam <samuel.lima@sptech.school>
2024-04-27 23:12:27 -0300

    Subindo o quiz com a lógica pronta e um pouco do css

---
+++ questions.js
@@ -0,0 +1,114 @@
+
+let questions = [{
+    numb: 1,
+    question: "Quem é o protagonista principal de Dragon Ball Z?",
+    answer: "Goku",
+    options: [
+      "Vegeta",
+      "Goku",
+      "Gohan",
+      "Piccolo"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual é a transformação de Goku que aumenta sua força e velocidade?",
+    answer: "Super Saiyajin",
+    options: [
+      "Super Saiyajin",
+      "Super Saiyajin Blue",
+      "Super Saiyajin 2",
+      "Super Saiyajin 3"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Quem é o príncipe da raça Saiyajin?",
+    answer: "Vegeta",
+    options: [
+      "Goku",
+      "Raditz",
+      "Nappa",
+      "Vegeta"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Qual é o nome da raça alienígena de Goku e Vegeta?",
+    answer: "Saiyajin",
+    options: [
+      "Namekuseijin",
+      "Terráqueo",
+      "Saiyajin",
+      "Androide"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Qual é a técnica de Goku que concentra sua energia em uma esfera?",
+    answer: "Kamehameha",
+    options: [
+      "Kaioken",
+      "Genki Dama",
+      "Final Flash",
+      "Kamehameha"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Quem é o vilão principal na Saga dos Saiyajins?",
+    answer: "Vegeta",
+    options: [
+      "Freeza",
+      "Cell",
+      "Majin Boo",
+      "Vegeta"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Qual é o planeta de origem de Goku?",
+    answer: "Planeta Vegeta",
+    options: [
+      "Planeta Terra",
+      "Planeta Namekusei",
+      "Planeta Vegeta",
+      "Planeta Kaio"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Quem é o criador das Esferas do Dragão?",
+    answer: "Kame-Sen'nin (Mestre Kame)",
+    options: [
+      "Piccolo Daimaoh",
+      "Guru",
+      "Kame-Sen'nin (Mestre Kame)",
+      "Mr. Popo"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Qual é o nome da fusão entre Goku e Vegeta?",
+    answer: "Gogeta",
+    options: [
+      "Vegetto",
+      "Gogeta",
+      "Gotenks",
+      "Gokule"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Qual é o objetivo principal de Goku em Dragon Ball Z?",
+    answer: "Se tornar o mais forte de todos",
+    options: [
+      "Proteger a Terra",
+      "Se tornar o mais forte de todos",
+      "Encontrar todas as Esferas do Dragão",
+      "Derrotar Freeza"
+    ]
+  }
+  ]
+  
+  
