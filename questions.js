02c6c3c3cf7eb64d60d15b1e994ed5e47d6f7349
Lzsaam <samuel.lima@sptech.school>
2024-05-21 21:59:00 -0300

    quiz finalizado

--- questions.js
+++ questions.js
@@ -1,114 +1,111 @@
-
 let questions = [{
-    numb: 1,
-    question: "Quem é o protagonista principal de Dragon Ball Z?",
-    answer: "Goku",
-    options: [
-      "Vegeta",
-      "Goku",
-      "Gohan",
-      "Piccolo"
-    ]
-  },
-  {
-    numb: 2,
-    question: "Qual é a transformação de Goku que aumenta sua força e velocidade?",
-    answer: "Super Saiyajin",
-    options: [
-      "Super Saiyajin",
-      "Super Saiyajin Blue",
-      "Super Saiyajin 2",
-      "Super Saiyajin 3"
-    ]
-  },
-  {
-    numb: 3,
-    question: "Quem é o príncipe da raça Saiyajin?",
-    answer: "Vegeta",
-    options: [
-      "Goku",
-      "Raditz",
-      "Nappa",
-      "Vegeta"
-    ]
-  },
-  {
-    numb: 4,
-    question: "Qual é o nome da raça alienígena de Goku e Vegeta?",
-    answer: "Saiyajin",
-    options: [
-      "Namekuseijin",
-      "Terráqueo",
-      "Saiyajin",
-      "Androide"
-    ]
-  },
-  {
-    numb: 5,
-    question: "Qual é a técnica de Goku que concentra sua energia em uma esfera?",
-    answer: "Kamehameha",
-    options: [
-      "Kaioken",
-      "Genki Dama",
-      "Final Flash",
-      "Kamehameha"
-    ]
-  },
-  {
-    numb: 6,
-    question: "Quem é o vilão principal na Saga dos Saiyajins?",
-    answer: "Vegeta",
-    options: [
-      "Freeza",
-      "Cell",
-      "Majin Boo",
-      "Vegeta"
-    ]
-  },
-  {
-    numb: 7,
-    question: "Qual é o planeta de origem de Goku?",
-    answer: "Planeta Vegeta",
-    options: [
-      "Planeta Terra",
-      "Planeta Namekusei",
-      "Planeta Vegeta",
-      "Planeta Kaio"
-    ]
-  },
-  {
-    numb: 8,
-    question: "Quem é o criador das Esferas do Dragão?",
-    answer: "Kame-Sen'nin (Mestre Kame)",
-    options: [
-      "Piccolo Daimaoh",
-      "Guru",
-      "Kame-Sen'nin (Mestre Kame)",
-      "Mr. Popo"
-    ]
-  },
-  {
-    numb: 9,
-    question: "Qual é o nome da fusão entre Goku e Vegeta?",
-    answer: "Gogeta",
-    options: [
-      "Vegetto",
-      "Gogeta",
-      "Gotenks",
-      "Gokule"
-    ]
-  },
-  {
-    numb: 10,
-    question: "Qual é o objetivo principal de Goku em Dragon Ball Z?",
-    answer: "Se tornar o mais forte de todos",
-    options: [
-      "Proteger a Terra",
-      "Se tornar o mais forte de todos",
-      "Encontrar todas as Esferas do Dragão",
-      "Derrotar Freeza"
-    ]
-  }
+  numb: 1,
+  question: "Qual agente de Valorant é um duelista com habilidades de regeneração?",
+  answer: "Phoenix",
+  options: [
+    "Jett",
+    "Phoenix",
+    "Reyna",
+    "Yoru"
   ]
-  
-  
+},
+{
+  numb: 2,
+  question: "Qual agente pode curar seus aliados com uma habilidade chamada Healing Orb?",
+  answer: "Sage",
+  options: [
+    "Breach",
+    "Sage",
+    "Skye",
+    "Viper"
+  ]
+},
+{
+  numb: 3,
+  question: "Qual agente é conhecido por suas habilidades de infiltração e criação de portais?",
+  answer: "Yoru",
+  options: [
+    "Cypher",
+    "Omen",
+    "Yoru",
+    "Jett"
+  ]
+},
+{
+  numb: 4,
+  question: "Qual agente usa uma torreta como uma de suas habilidades?",
+  answer: "Killjoy",
+  options: [
+    "Killjoy",
+    "Brimstone",
+    "Cypher",
+    "Breach"
+  ]
+},
+{
+  numb: 5,
+  question: "Qual é a habilidade suprema de Jett que permite lançar facas afiadas?",
+  answer: "Blade Storm",
+  options: [
+    "Updraft",
+    "Tailwind",
+    "Cloudburst",
+    "Blade Storm"
+  ]
+},
+{
+  numb: 6,
+  question: "Qual agente possui a habilidade de 'Recon Bolt' para revelar inimigos?",
+  answer: "Sova",
+  options: [
+    "Sova",
+    "Cypher",
+    "KAY/O",
+    "Breach"
+  ]
+},
+{
+  numb: 7,
+  question: "Qual agente tem a habilidade de 'Snake Bite'?",
+  answer: "Viper",
+  options: [
+    "Viper",
+    "Phoenix",
+    "Omen",
+    "Brimstone"
+  ]
+},
+{
+  numb: 8,
+  question: "Qual agente pode implantar um 'Stim Beacon' para aumentar a taxa de disparo dos aliados?",
+  answer: "Brimstone",
+  options: [
+    "Phoenix",
+    "Brimstone",
+    "Sage",
+    "Breach"
+  ]
+},
+{
+  numb: 9,
+  question: "Qual agente usa a habilidade 'Shock Dart'?",
+  answer: "Sova",
+  options: [
+    "Raze",
+    "Killjoy",
+    "Sova",
+    "Breach"
+  ]
+},
+{
+  numb: 10,
+  question: "Qual agente tem a habilidade de invocar um lobo de busca?",
+  answer: "Skye",
+  options: [
+    "Reyna",
+    "Skye",
+    "Sage",
+    "Omen"
+  ]
+}
+];
