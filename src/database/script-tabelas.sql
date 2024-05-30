3b32bb124e1168e24e6ea641925c0c648efac034
Lzsaam <samuel.lima@sptech.school>
2024-05-30 15:02:55 -0300

    atualizando o banco de dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -16,3 +16,55 @@ CREATE TABLE usuario (
 	email VARCHAR(50),
 	senha VARCHAR(50)
 );
+
+select * from usuario;
+
+CREATE TABLE Resposta (
+	idResposta INT PRIMARY KEY AUTO_INCREMENT,
+    pontos INT,
+    acertos INT,
+	fkUsuario int, 
+	foreign key (fkUsuario) REFERENCES usuario (id)
+);
+
+SELECT * FROM USUARIO
+JOIN Resposta ON id = fkUsuario ORDER BY pontos;
+
+SELECT * FROM Resposta;
+
+
+create table resposta_certa(
+idRespostaCerta int primary key auto_increment,
+correta varchar(45)
+);
+
+create table questoes(
+idRanking int  auto_increment,
+fkResposta int,
+	foreign key (fkresposta)
+		references resposta (idResposta),
+fkUsuario int, 
+	foreign key (fkUsuario)
+		references usuario(id),
+primary key(idRanking, fkResposta, fkUsuario),
+score int,
+acerto varchar(45)
+);
+
+
+create table ranking (
+idRaNKING int primary key auto_increment,
+fkUsuario int,
+foreign key (fkUsuario) references usuario(id),
+score int,
+acertos int
+);
+
+select * from usuario;	
+
+
+SELECT * FROM RANKING ORDER BY SCORE;
+
+
+
+
